cec1c94d89e215891b49bce953672936291456a7
JJ <jj.vega86@gmail.com>
2021-01-04 14:30:55 -0500

    Added repoContext to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const repoContext = require("./repository/repository-wrapper");
 const express = require('express'); // import express package into index.js and return a function stored in const 'express'
 const app = express(); // store express function in app - this is industry convention
 
