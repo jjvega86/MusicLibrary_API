b8b812cac4b28cfbc9eac91ee3eb29fd480ccbcd
JJ <jj.vega86@gmail.com>
2021-01-04 14:14:14 -0500

    Imported express package into index.js, stored in app, ran a .listen

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const express = require('express'); // import express package into index.js and return a function stored in const 'express'
 const app = express(); // store express function in app - this is industry convention
 
+app.listen(3000, function (){
+    console.log("Server started. Listening on port 3000.");
+});
