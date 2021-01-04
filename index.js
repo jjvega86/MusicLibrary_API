c7c1061cbb3a771367e66fcf393cf2a223b0857a
JJ <jj.vega86@gmail.com>
2021-01-04 12:19:22 -0500

    Imported express and stored in app

---
+++ index.js
@@ -0,0 +1,3 @@
+const express = require('express'); // import express package into index.js and return a function stored in const 'express'
+const app = express(); // store express function in app - this is industry convention
+
