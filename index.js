cd6cbdf7717153932bdda69256bdbd25f38fd20c
JJ <jj.vega86@gmail.com>
2021-01-04 15:32:43 -0500

    Added POST new product endpoint and tested successful in Postman

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const repoContext = require("./repository/repository-wrapper");
 const express = require('express'); // import express package into index.js and return a function stored in const 'express'
 const app = express(); // store express function in app - this is industry convention
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.listen(3000, function (){
     console.log("Server started. Listening on port 3000.");
 });
@@ -19,5 +22,14 @@ app.get("/api/products/:id", (req,res) => {
 
 // middleware: middleware functions are functions that have access to the req and res objects in hte application's request-response cycle
 // can terminate HTTP request or pass it on to another middleware using 'next'
+
+app.post("/api/products", (req,res) => {
+    let newProduct = req.body;
+    let addedProduct = repoContext.products.createProduct(newProduct);
+    res.send(addedProduct);
+});
+
+// IMPORTANT: when making a POST request, don't include primary key information. This is handled automatically on the server side.
+
 
 
